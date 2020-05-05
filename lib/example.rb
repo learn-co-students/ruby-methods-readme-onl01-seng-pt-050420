afbc92aa29245ec15210c13ede74c223308caafe
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-05 00:33:09 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,14 +1,3 @@
-# def say_hello_world_ten_times
-def say_hello_ten_times
-  phrase = "Hello World!"
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
+def greeting
+  #puts "Hello WOrld"
 end
