c0a7ff7973bebf54ba85fa5d709ee20fd57a551c
Deborah Santillan <s.debb@yahoo.com>
2020-05-06 15:53:27 +0000

    Done.

---
+++ ruby lib/example.rb
@@ -0,0 +1,29 @@
+phrase = "Hello World!"
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase 
+puts phrase
+puts phrase
+puts phrase
+puts phrase 
+puts phrase
+
+def say_hello_world_ten_times
+phrase = "Hello World!"
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+end
+
+def greeting 
+  puts "Hello world!" 
+end 
