e339d6c066fd8ef6468833883b31d35b168d6682
Allison Wilson <amariew94@outlook.com>
2020-05-07 04:18:22 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,4 +1,24 @@
+def greeting
+  puts "hello world"
+end
+
+phrase = "hello world"
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+
 # Use this space to code along with the readme
+def greeting
+  puts "hellow world"
+end
+greeting
 
 phrase = "Hello World!"
 puts phrase
